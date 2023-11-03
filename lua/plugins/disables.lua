ee4c5135131930d29ab0e8a1158209404609db27
Kent Chiu <kent.cwg@gmail.com>
2023-11-03 13:32:38 +0800

    disable session

--- lua/plugins/disables.lua
+++ lua/plugins/disables.lua
@@ -7,4 +7,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "nvim-pack/nvim-spectre", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
 }
