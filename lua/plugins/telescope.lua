d83c0b30211aab2b9c11b6e7f8ae8bf30f5b5c62
Kent Chiu <kent.cwg@gmail.com>
2023-12-30 22:04:46 +0800

    recent file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,12 @@ return
   {
     "nvim-telescope/telescope.nvim",
     keys = {
-      { "<leader>fF", "<cmd>cd %:p:h<cr><cmd>Telescope find_files<cr>", desc = "Find Files(cwd)" },
-      { "<leader>sG", "<cmd>cd %:p:h<cr><cmd>Telescope live_grep<cr>",  desc = "Grep(cwd)" },
+      -- { "<leader>ff", "<cmd>cd %:p:h<cr><cmd>Telescope find_files<cr>", desc = "Find Files(cwd)" },
+      -- { "<leader>fF", "<cmd>Telescope find_files<cr>",                  desc = "Find Files(ROOT)" },
+      -- { "<leader>sG", "<cmd>cd %:p:h<cr><cmd>Telescope live_grep<cr>", desc = "Grep(cwd)" },
+      -- { "<leader>fR", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
+      { "<leader>fr", "<cmd>Telescope oldfiles only_cwd=true<cr>", desc = "Find Files(cwd)" },
+      { "<leader>fR", "<cmd>Telescope oldfiles<cr>",               desc = "Recent" },
     },
     opts = {
       defaults = {
