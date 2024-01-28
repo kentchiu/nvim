6d303053053f5ddd1eb11a685f308103f3a09205
Kent Chiu <kent.cwg@gmail.com>
2024-01-28 18:22:42 +0800

    disable plugin for dev

---
+++ lua/plugins/disables-dev.lua
@@ -0,0 +1,8 @@
+return {
+  { "kentchiu/gemini.nvim",    enabled = false },
+  { "robitx/gp.nvim",          enabled = false },
+  { "keaising/im-select.nvim", enabled = false },
+  { "nvim.neotest/neotest",    enabled = false },
+  { "williamboman/mason.nvim", enabled = false },
+  { "zbirenbaum/copilot.lua", enabled = false },
+}
