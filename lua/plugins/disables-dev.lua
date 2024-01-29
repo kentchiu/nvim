c46a98750940f498a55b6ed94aeb303fbf046131
Kent Chiu <kent.cwg@gmail.com>
2024-01-30 00:31:43 +0800

    add dev check

--- lua/plugins/disables-dev.lua
+++ lua/plugins/disables-dev.lua
@@ -1,8 +1,12 @@
+local env = vim.env
+-- only kent's dev env has GOOGLE_API_KEY
+if env.GOOGLE_API_KEY ~= nil then return {} end
+
 return {
   { "kentchiu/gemini.nvim",    enabled = false },
   { "robitx/gp.nvim",          enabled = false },
   { "keaising/im-select.nvim", enabled = false },
   { "nvim.neotest/neotest",    enabled = false },
   { "williamboman/mason.nvim", enabled = false },
-  { "zbirenbaum/copilot.lua", enabled = false },
+  { "zbirenbaum/copilot.lua",  enabled = false },
 }
