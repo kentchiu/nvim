0d8c0c24fbe7632352e4fefe9a8404aea6900db6
Kent Chiu <kent.cwg@gmail.com>
2024-03-19 17:41:35 +0800

    cmdlet

--- lua/plugins/disables.lua
+++ lua/plugins/disables.lua
@@ -1,8 +1,8 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim",  enabled = false },
   { "nvimdev/dashboard-nvim",       enabled = false },
-  { "rcarriga/nvim-notify",         enabled = false },
-  { "folke/noice.nvim",             enabled = false },
+  -- { "rcarriga/nvim-notify",         enabled = false },
+  -- { "folke/noice.nvim",             enabled = false },
   { "akinsho/bufferline.nvim",      enabled = false },
   { "nvim-pack/nvim-spectre",       enabled = false },
   { "folke/persistence.nvim",       enabled = false },
