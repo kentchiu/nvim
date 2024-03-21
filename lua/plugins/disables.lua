5b4b8b64b765d57226d353c95044ef39af0f83dc
Kent Chiu <kent.cwg@gmail.com>
2024-03-21 22:34:41 +0800

    disable notify

--- lua/plugins/disables.lua
+++ lua/plugins/disables.lua
@@ -1,7 +1,7 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim",  enabled = false },
   { "nvimdev/dashboard-nvim",       enabled = false },
-  -- { "rcarriga/nvim-notify",         enabled = false },
+  { "rcarriga/nvim-notify",         enabled = false },
   -- { "folke/noice.nvim",             enabled = false },
   { "akinsho/bufferline.nvim",      enabled = false },
   { "nvim-pack/nvim-spectre",       enabled = false },
