3e6d314d49954d82c95fa8a8540d886e1721b6b6
Kent Chiu <kent.cwg@gmail.com>
2024-04-06 11:31:47 +0800

    debug print

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -1,29 +1,34 @@
 return {
   {
     'andrewferrier/debugprint.nvim',
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      local opts = {
-        create_keymaps = false,
-        print_tag = "🟥"
-      }
+    opts = {
+      print_tag = "🟥",
+      keymaps = {
+        normal = {
+          plain_below = "<leader>pp",
+          plain_above = "<leader>pP",
+          variable_below = "<leader>pv",
+          variable_above = "<leader>pV",
+          variable_below_alwaysprompt = nil,
+          variable_above_alwaysprompt = nil,
+          textobj_below = "<leader>po",
+          textobj_above = "<leader>pO",
+          toggle_comment_debug_prints = nil,
+          delete_debug_prints = nil,
+        },
+        visual = {
+          variable_below = "<leader>pv",
+          variable_above = "<leader>pV",
+        },
+      },
+      commands = {
+        toggle_comment_debug_prints = "ToggleCommentDebugPrints",
+        delete_debug_prints = "DeleteDebugPrints",
+      },
+    },
+    config = function(_, opts, x)
       require("debugprint").setup(opts)
 
-      vim.keymap.set("n", "<Leader>pp", function() return require('debugprint').debugprint() end,
-        { expr = true, desc = "Print debug belowe", })
-      vim.keymap.set("n", "<Leader>pP", function() return require('debugprint').debugprint({ above = true }) end,
-        { expr = true, desc = "Plain debug above", })
-      vim.keymap.set({ "n", "x" }, "<Leader>pv",
-        function() return require('debugprint').debugprint({ variable = true }) end,
-        { expr = true, desc = "Variable debug below", })
-      vim.keymap.set({ "n", "x" }, "<Leader>pV",
-        function() return require('debugprint').debugprint({ above = true, variable = true }) end,
-        { expr = true, desc = "Variable debug above", })
-      vim.keymap.set("n", "<Leader>po", function() return require('debugprint').debugprint({ motion = true }) end,
-        { expr = true, desc = "Text-obj-selected below" })
-      vim.keymap.set("n", "<Leader>pO",
-        function() return require('debugprint').debugprint({ motion = true, above = true }) end,
-        { expr = true, desc = "Text-obj-selected above" })
       vim.keymap.set("n", "<Leader>pd", "<CMD>DeleteDebugPrints<CR>", { desc = "Delete Prints." })
       vim.keymap.set("n", "<Leader>pl", function()
         vim.cmd("vimgrep " .. "/" .. opts.print_tag .. "/g `git ls-files`")
