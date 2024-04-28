fa5b4c3f102f378eddec4cc55cfef4081f7f136c
Kent Chiu <kent.cwg@gmail.com>
2024-04-28 14:23:46 +0800

    obsidian

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,8 +4,8 @@
 
 vim.opt.relativenumber = false -- Relative line numbers
 vim.opt.listchars = { space = "·", tab = "⇥ ", eol = "↵" }
-vim.opt.swapfile = false       -- disable swapfiles
-vim.opt.conceallevel = 0       -- disable conceal
+vim.opt.swapfile = false -- disable swapfiles
+-- vim.opt.conceallevel = 0       -- disable conceal
 -- Enable LazyVim auto format
 -- vim.g.autoformat = false
 -- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
