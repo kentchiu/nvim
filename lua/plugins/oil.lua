698a62efffedca10011edf941ad09d28b6cafab4
Kent Chiu <kent.cwg@gmail.com>
2024-05-18 13:50:10 +0800

    oil show hidden

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,14 +1,18 @@
 return {
   {
-    'stevearc/oil.nvim',
+    "stevearc/oil.nvim",
     opts = {},
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("oil").setup({
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
-        vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+        view_options = {
+          -- Show files and directories that start with "."
+          show_hidden = true,
+        },
       })
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
-  }
+  },
 }
