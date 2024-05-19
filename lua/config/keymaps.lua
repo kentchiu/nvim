8f8901534b674b1f7da19105ebdc7230ad5ff872
Kent Chiu <kent.cwg@gmail.com>
2024-05-19 15:01:38 +0800

    add window only keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,10 +5,6 @@
 -- [[ Basic Keymaps ]]
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape" })
 
--- make page forward/backwoard firendly
--- vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll window Downwards And Center It" })
--- vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll window Upwards And Center It" })
-
 -- remove ^M
 vim.keymap.set("n", "<leader>bc", "<cmd>%s/\\r$//g<cr>", { desc = "remove ^M" })
 
@@ -26,23 +22,6 @@ vim.keymap.set({ "n", "v", "o", "x" }, "M", "gm") --"middle of line"
 vim.keymap.set("n", "<leader>fP", '<cmd>let @+ = expand("%")<CR>', { desc = "Copy File Name" })
 vim.keymap.set("n", "<leader>fp", '<cmd>let @+ = expand("%:p")<CR>', { desc = "Copy File Path" })
 vim.keymap.set("n", "<leader>fa", '<cmd>let @+ = expand("%:.")<CR>', { desc = "Copy Relative File Path" })
---
-
--- Obsidian
--- vim.keymap.set(
---   "n",
---   "<leader>oc",
---   "<cmd>lua require('obsidian').util.toggle_checkbox()<CR>",
---   { desc = "Obsidian Check Checkbox" }
--- )
--- vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTemplate<CR>", { desc = "Insert Obsidian Template" })
--- vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open in Obsidian App" })
--- vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show ObsidianBacklinks" })
--- vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show ObsidianLinks" })
--- vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Create New Note" })
--- vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
--- vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
--- misc
 
 -- Inspect TreeSitter
 -- vim.keymap.set("n", "<leader>uI", "<cmd>InspectTree<cr>", { desc = "Inspect Tree" })
@@ -50,6 +29,7 @@ vim.keymap.set("n", "<leader>fa", '<cmd>let @+ = expand("%:.")<CR>', { desc = "C
 vim.keymap.del("n", "<leader>ft")
 vim.keymap.set("n", "<leader>ft", "<cmd>vsplit<CR><cmd>terminal<CR>", { desc = "Termial" })
 
+vim.keymap.set("n", "<leader>wo", "<C-W>o", { desc = "Make Only" })
 -- vim.keymap.del("n", "<leader>ft")
 -- vim.keymap.del("n", "<leader>fT")
 -- vim.keymap.del("n", "<c-/>")
