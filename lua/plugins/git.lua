a324af0b21066f388c42b10dcd91b08e9e2a923f
Kent Chiu <kent.cwg@gmail.com>
2024-06-26 22:28:46 +0800

    git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,8 +7,7 @@ return {
       -- vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit<CR>', { noremap = true, silent = false, desc = "Git vertical diff" })
       -- vim.keymap.set('n', '<leader>gr', ':Gread<CR>', { noremap = true, silent = true, desc = "Git read" })
       -- vim.keymap.set('n', '<leader>gw', ':Gwrite<CR>', { noremap = true, silent = true, desc = "Git write" })
-      -- vim.keymap.set("n", "<leader>gl", ":Gclog<CR>", { desc = "Commit Log in Qfix" })
-      -- vim.keymap.set("n", "<leader>gb", "<cmd>DiffviewFileHistory<cr>", { desc = "Branch History" })
+      vim.keymap.set({"n"}, "<leader>gl", ":Gclog<CR>", { desc = "Commit Log in Qfix" })
     end,
   },
   {
