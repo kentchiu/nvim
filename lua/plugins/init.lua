1b454e4a284fd504281c8a2979ec0ca34e826a0b
Kent Chiu <kent.cwg@gmail.com>
2024-09-08 01:15:06 +0800

    dap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -58,6 +58,17 @@ local others = {
       },
     },
   },
+  {
+    "OXY2DEV/helpview.nvim",
+    lazy = false, -- Recommended
+
+    -- In case you still want to lazy load
+    -- ft = "help",
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
 }
 
 local disables = {
