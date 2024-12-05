47f59adb9d68840bcb05f284edafced493c34d41
Kent Chiu <kent.cwg@gmail.com>
2024-12-05 15:12:29 +0800

    add aider

--- lua/plugins/playground.lua
+++ lua/plugins/playground.lua
@@ -1,1 +1,23 @@
-return {}
+return {
+  {
+    "kentchiu/nvim-aider",
+    cmd = {
+      "AiderTerminalToggle",
+    },
+    keys = {
+      { "<leader>add", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
+      { "<leader>ads", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+      { "<leader>adc", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
+      { "<leader>adb", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
+      { "<leader>ad+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
+      { "<leader>ad-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
+    },
+    dependencies = {
+      "folke/snacks.nvim",
+      "nvim-telescope/telescope.nvim",
+      --- The below dependencies are optional
+      "catppuccin/nvim",
+    },
+    config = true,
+  },
+}
