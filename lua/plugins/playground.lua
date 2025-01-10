bcd79ff20fb2ca98c60d3d083bf666e0d276940a
Kent Chiu <kent.cwg@gmail.com>
2025-01-10 19:37:09 +0800

    comment out nvim-aider

--- lua/plugins/playground.lua
+++ lua/plugins/playground.lua
@@ -5,4 +5,25 @@ return {
     lazy = false,
     enable = true,
   },
+  -- {
+  --   "GeorgesAlkhouri/nvim-aider",
+  --   cmd = {
+  --     "AiderTerminalToggle",
+  --   },
+  --   keys = {
+  --     { "<leader>a/", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
+  --     { "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+  --     { "<leader>ac", "<cmd>AiderQuickSendCommand<cr>", desc = "Send Command To Aider" },
+  --     { "<leader>ab", "<cmd>AiderQuickSendBuffer<cr>", desc = "Send Buffer To Aider" },
+  --     { "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
+  --     { "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
+  --   },
+  --   dependencies = {
+  --     "folke/snacks.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --     --- The below dependencies are optional
+  --     "catppuccin/nvim",
+  --   },
+  --   config = true,
+  -- },
 }
