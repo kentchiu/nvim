2ecc0db4f4fb955b660d2753d0053225bc722387
Kent Chiu <kent.cwg@gmail.com>
2025-01-19 20:22:23 +0800

    disable bufferline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -55,7 +55,6 @@ local disables = {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- { "nvimdev/dashboard-nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = true },
 }
 --
 --
