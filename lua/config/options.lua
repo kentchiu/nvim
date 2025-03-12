4802528dd4ed110629a7be733ca85ea0328fb470
Kent Chiu <kent.cwg@gmail.com>
2025-03-13 00:59:03 +0800

    diff opt

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,4 +23,15 @@ vim.g.lazyvim_picker = "snacks"
 -- in this case.
 -- vim.g.lazyvim_blink_main = false
 --
-vim.opt.diffopt = "filler,internal,closeoff,algorithm:histogram,context:5,linematch:60"
+-- vim.opt.diffopt = "filler,internal,closeoff,algorithm:histogram,context:5,linematch:60"
+-- example here: https://vimways.org/2018/the-power-of-diff/
+vim.opt.diffopt = {
+  "internal", -- use internal diff
+  "filler", -- fill diff with empty row which make diff more Intuitive
+  "closeoff", -- auto close diff when buffer close
+  "context:12", -- diff conext line
+  "algorithm:histogram", -- suit for small chunk
+  "linematch:200",
+  "indent-heuristic", -- smart indent
+  -- "iwhite", -- this one, it doesn't fit all cases.
+}
