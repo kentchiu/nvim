d9aceca7a405e8538be08a2535dd90aa67bfda94
Kent Chiu <kent.cwg@gmail.com>
2025-10-08 22:08:13 +0800

    add kent

---
+++ lua/plugins/kent.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "kentchiu/aider.nvim",
+    enabled = false,
+    dir = "~/dev/kent/aider.nvim",
+    dev = true,
+    opts = {
+      logger = {
+        level = "OFF",
+      },
+    },
+  },
+  {
+    "kentchiu/tmux-send.nvim",
+    dir = "~/dev/kent/tmux-send.nvim",
+    lazy = false,
+    enabled = true,
+    dev = true,
+    opts = {
+      logger = {
+        level = "OFF",
+      },
+    },
+  },
+  {
+    "kentchiu/d2.nvim",
+    dir = "~/dev/kent/d2.nvim",
+    lazy = false,
+    dev = true,
+    opts = {
+      logger = {
+        level = "OFF",
+      },
+    },
+  },
+  {
+    "kentchiu/olink.nvim",
+    dir = "~/dev/kent/olink.nvim",
+    lazy = false,
+    dev = true,
+  },
+}
