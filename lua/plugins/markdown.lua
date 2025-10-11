64d91af502f79796ed9c9f1dfa93281d7472604e
Kent Chiu <kent.cwg@gmail.com>
2025-10-11 16:54:53 +0800

    chore(markdown): 🔧 disable sync scroll in markdown preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -13,6 +13,9 @@ return {
     init = function()
       vim.g.mkdp_theme = "light"
       vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_preview_options = {
+        disable_sync_scroll = 1,
+      }
     end,
   },
   -- {
