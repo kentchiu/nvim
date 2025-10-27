fc1948b491cf3baa2925f0ab305d44c9b7910e02
Kent Chiu <kent.cwg@gmail.com>
2025-10-27 18:54:08 +0800

    fix for ormerchy

--- lua/plugins/kent.lua
+++ lua/plugins/kent.lua
@@ -34,6 +34,12 @@ return {
     },
   },
   {
+    "ravsii/tree-sitter-d2",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    version = "*", -- use the latest git tag instead of main
+    build = "make nvim-install",
+  },
+  {
     "kentchiu/olink.nvim",
     enabled = false,
     -- dir = "~/dev/kent/olink.nvim",
