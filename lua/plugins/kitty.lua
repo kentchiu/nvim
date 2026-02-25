ed76fb87ad5de3d20b995c8e9ad666bcbbd1bd9d
Kent Chiu <kent.cwg@gmail.com>
2026-02-26 00:13:51 +0800

    🔧 chore(kitty-scrollback): disable yank register in paste window

--- lua/plugins/kitty.lua
+++ lua/plugins/kitty.lua
@@ -12,7 +12,14 @@ return {
     -- version = '*', -- latest stable version, may have breaking changes if major version changed
     -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
     config = function()
-      require("kitty-scrollback").setup()
+      require("kitty-scrollback").setup({
+        -- global options applied to all configs (index [1])
+        {
+          paste_window = {
+            yank_register_enabled = false,
+          },
+        },
+      })
     end,
   },
 }
